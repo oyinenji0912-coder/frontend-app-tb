83ad1c47f3e039e9f68ca33ae633e41c4d074604
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2026-09-08 14:49:14 +0700

    push

---
+++ gitpull.sh
@@ -0,0 +1,5 @@
+sudo git reset --hard
+sudo git pull https://github.com/oyinenji0912-coder/frontend-app-tb.git main
+sudo npm install
+sudo npm run build
+sudo cp -r dist/* .
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/kawal-tb",
   server: {
-    host: '0.0.0.0',
-    port: 5173,
+    host: "0.0.0.0",
+    // port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8766',
+      "/api": {
+        target: "http://10.15.102.73:8766",
         changeOrigin: true,
       },
     },
   },
-})
+});
